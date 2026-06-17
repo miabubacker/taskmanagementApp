77bcb461624ebfe33c24d45e1bad19f4c5c10b1c
Abubacker <miabubacker98@gmail.com>
2026-06-18 00:01:33 +0530

    backend setup

---
+++ Backend/config/db.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/taskmanagement", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
