4310afac8ba4b35ca37ad84b734e4e1002f70230
Abubacker <miabubacker98@gmail.com>
2026-06-18 23:14:21 +0530

    backend serverAndMongodbConnection

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+connectDB();
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  
 });
+
